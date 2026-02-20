70f447baa74e233e712ea370c288c1ee16aac0c0
Abdisa.dev <meetabdisa@gmail.com>
2026-02-20 19:17:08 +0300

    enable cross-domain cookies for production

--- backend/utils/createToken.ts
+++ backend/utils/createToken.ts
@@ -14,7 +14,7 @@ const generateToken = (res: Response, userId: string): string => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV !== "development" ? "none" : "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 
