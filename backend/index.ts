6551ded87a1ada6137e1412dcfd49359f544aaff
Abdisa.dev <meetabdisa@gmail.com>
2026-02-20 18:51:49 +0300

    deployment setup

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -18,6 +19,13 @@ const port: string | number = process.env.PORT || 5001;
 connectDB();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
